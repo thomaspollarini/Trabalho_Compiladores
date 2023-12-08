fec4d2a8f6a3a1021d7267ad5785a6dad09e89b3
Thomas <thomassantospallarini@gmail.com>
2023-12-08 03:11:01 -0300

    follow token sincronismo

--- sintatico.py
+++ sintatico.py
@@ -25,8 +25,35 @@ class Sintatico:
         self.deuErro = False
         # campos utilizados no modo panico
         self.modoPanico = False
-        self.tokensDeSincronismo = [tt.PVIRG, tt.FIMARQ]
-
+        self.follow = {'PROG' : [tt.FIMARQ],
+                       'DECLS' : [tt.ABRECH],
+                       'LIST_DECLS' : [tt.ABRECH],
+                       'D' : [tt.ABRECH],
+                       'DECL_TIPO' : [tt.ID, tt.ABRECH],
+                       'LIST_ID' : [tt.DPONTOS, tt.FECHAPAR],
+                       'E' : [tt.DPONTOS, tt.FECHAPAR],
+                       'TIPO' : [tt.PVIRG],
+                       'C_COMP' : [tt.FIMARQ,tt.IF,tt.WHILE,tt.READ,tt.WRITE,tt.ID,tt.FECHACH],
+                       'LISTA_COMANDOS' : [tt.FECHACH],
+                       'G' : [tt.FECHACH],
+                       'COMANDOS' : [tt.IF,tt.WHILE,tt.READ,tt.WRITE,tt.ID,tt.FECHACH],
+                       'SE' : [tt.IF,tt.WHILE,tt.READ,tt.WRITE,tt.ID,tt.FECHACH],
+                       'H' : [tt.IF,tt.WHILE,tt.READ,tt.WRITE,tt.ID,tt.FECHACH],
+                       'ENQUANTO' : [tt.IF,tt.WHILE,tt.READ,tt.WRITE,tt.ID,tt.FECHACH],
+                       'LEIA' : [tt.IF,tt.WHILE,tt.READ,tt.WRITE,tt.ID,tt.FECHACH],
+                       'ATRIBUICAO' : [tt.IF,tt.WHILE,tt.READ,tt.WRITE,tt.ID,tt.FECHACH],
+                       'ESCREVA' : [tt.IF,tt.WHILE,tt.READ,tt.WRITE,tt.ID,tt.FECHACH],
+                       'LIST_W' : [tt.FECHAPAR],
+                       'L' : [tt.FECHAPAR],
+                       'ELEM_W' : [tt.VIRG,tt.FECHAPAR],
+                       'EXPR' : [tt.PVIRG,tt.VIRG,tt.FECHAPAR],
+                       'P' : [tt.PVIRG,tt.VIRG,tt.FECHAPAR],
+                       'SIMPLES' : [tt.OPREL,tt.PVIRG,tt.VIRG,tt.FECHAPAR],
+                       'R' : [tt.OPREL,tt.PVIRG,tt.VIRG,tt.FECHAPAR],
+                       'TERMO' : [tt.OPAD,tt.OPREL,tt.PVIRG,tt.VIRG,tt.FECHAPAR],
+                       'S' : [tt.OPAD,tt.OPREL,tt.PVIRG,tt.VIRG,tt.FECHAPAR],
+                       'FAT' : [tt.OPMUL,tt.OPAD,tt.OPREL,tt.PVIRG,tt.VIRG,tt.FECHAPAR],
+                       }
 
     #função que inicia a interpretação do arquivo
     def interprete(self, nomeArquivo):
@@ -38,7 +65,7 @@ class Sintatico:
             self.tokenAtual = self.lex.getToken()
             self.tabsimb = TabelaSimbolos()
             self.PROG()   #chama regra inicial da gramática
-            self.consome( tt.FIMARQ )
+            self.consome( tt.FIMARQ , "PROG")
             
             self.lex.fechaArquivo()
             return not self.deuErro
@@ -52,7 +79,7 @@ class Sintatico:
             return False
 
     #função que consome o token atual, caso seja igual ao token esperado
-    def consome(self, token):       
+    def consome(self, token, NaoTerminal):       
         if not self.modoPanico and self.tokenEsperadoEncontrado( token ):
             # tudo seguindo de acordo
             self.tokenAtual = self.lex.getToken()
@@ -66,14 +93,15 @@ class Sintatico:
                % (self.tokenAtual.linha, msg, self.tokenAtual.lexema))
             #quit()
             procuraTokenDeSincronismo = True
+            tokensDeSincronismo = [tt.PVIRG, tt.FIMARQ]
+            tokensDeSincronismo.extend(self.follow[NaoTerminal])
             while procuraTokenDeSincronismo:
                 self.tokenAtual = self.lex.getToken()
-                for tk in self.tokensDeSincronismo:
+                for tk in tokensDeSincronismo:
                     (const, msg) = tk
                     if self.tokenAtual.const == const:
                         # tokenAtual eh um token de sincronismo
                         procuraTokenDeSincronismo = False
-                        self.tokenAtual = self.lex.getToken()
                         break
         elif self.tokenEsperadoEncontrado(token):
             # chegou no ponto de sincronismo :)
@@ -96,16 +124,16 @@ class Sintatico:
 
     #PROG → program id pvirg DECLS C_COMP
     def PROG(self):                                 
-        self.consome(tt.PROGRAM)
-        self.consome(tt.ID)
-        self.consome(tt.PVIRG)
+        self.consome(tt.PROGRAM, 'PROG')
+        self.consome(tt.ID, 'PROG')
+        self.consome(tt.PVIRG, 'PROG')
         self.DECLS()
         self.C_COMP()
 
     #DECLS → var LIST_DECLS | λ
     def DECLS(self):
         if self.tokenEsperadoEncontrado(tt.VAR):
-            self.consome(tt.VAR)
+            self.consome(tt.VAR, 'DECLS')
             self.LIST_DECLS()
         else:
             pass
@@ -126,22 +154,22 @@ class Sintatico:
     def DECL_TIPO(self):
         nomes=[] #lista de para salvar ids
         self.LIST_ID(nomes)
-        self.consome(tt.DPONTOS)
+        self.consome(tt.DPONTOS, 'DECL_TIPO')
         valor =self.TIPO() #salva o tipo
         self.tabsimb.declaraIdent(nomes, valor,self.tokenAtual.linha) #declara os ids na tabela de simbolos
-        self.consome(tt.PVIRG)
+        self.consome(tt.PVIRG,  'DECL_TIPO')
         
     #LIST_ID → id E
     def LIST_ID(self, nomes=[]):
         if self.tokenEsperadoEncontrado(tt.ID):   #se for um id
             nomes.append(self.tokenAtual.lexema)  #adiciona na lista de ids
-        self.consome( tt.ID )
+        self.consome( tt.ID , 'LIST_ID')
         self.E(nomes)
 
     #E → virg LIST_ID | λ
     def E(self,nomes):
         if self.tokenEsperadoEncontrado(tt.VIRG):
-            self.consome(tt.VIRG)
+            self.consome(tt.VIRG, 'E')
             self.LIST_ID(nomes)
         else:
             pass
@@ -149,24 +177,24 @@ class Sintatico:
     #TIPO → int | real | bool | char
     def TIPO(self):
         if self.tokenEsperadoEncontrado( tt.INT ):
-            self.consome(tt.INT)
+            self.consome(tt.INT,'TIPO')
             return "int"
         elif self.tokenEsperadoEncontrado(tt.REAL):
-            self.consome(tt.REAL)
+            self.consome(tt.REAL,'TIPO')
             return "real"
         elif self.tokenEsperadoEncontrado(tt.BOOL):
-            self.consome(tt.BOOL)
+            self.consome(tt.BOOL,'TIPO')
             return "bool"
         else:
-            self.consome(tt.CHAR)
+            self.consome(tt.CHAR,'TIPO')
             return "char"
         
 
     #C_COMP → abrech LISTA_COMANDOS fechach
     def C_COMP(self):
-        self.consome(tt.ABRECH)
+        self.consome(tt.ABRECH, 'C_COMP')
         self.LISTA_COMANDOS()
-        self.consome(tt.FECHACH)
+        self.consome(tt.FECHACH, 'C_COMP')
 
     #LISTA_COMANDOS → COMANDOS G
     def LISTA_COMANDOS(self):
@@ -198,51 +226,51 @@ class Sintatico:
 
     #SE → if abrepar EXPR fechapar C_COMP H
     def SE(self):
-        self.consome(tt.IF)
-        self.consome(tt.ABREPAR)
+        self.consome(tt.IF, 'SE')
+        self.consome(tt.ABREPAR, 'SE')
         self.EXPR()
-        self.consome(tt.FECHAPAR)
+        self.consome(tt.FECHAPAR, 'SE')
         self.C_COMP()
         self.H()
         
     #H → else C_COMP | λ
     def H(self):
         if self.tokenEsperadoEncontrado(tt.ELSE):
-            self.consome(tt.ELSE)
+            self.consome(tt.ELSE, 'H')
             self.C_COMP()
         else:
             pass
         
     #ENQUANTO → while abrepar EXPR fechapar C_COMP
     def ENQUANTO(self):
-        self.consome(tt.WHILE)
-        self.consome(tt.ABREPAR)
+        self.consome(tt.WHILE, 'ENQUANTO')
+        self.consome(tt.ABREPAR, 'ENQUANTO')
         self.EXPR()
-        self.consome(tt.FECHAPAR)
+        self.consome(tt.FECHAPAR, 'ENQUANTO')
         self.C_COMP()
         
     #LEIA → read abrepar LIST_ID fechapar pvirg
     def LEIA(self):
-        self.consome(tt.READ)
-        self.consome(tt.ABREPAR)
+        self.consome(tt.READ, 'LEIA')
+        self.consome(tt.ABREPAR, 'LEIA')
         self.LIST_ID()
-        self.consome(tt.FECHAPAR)
-        self.consome(tt.PVIRG)
+        self.consome(tt.FECHAPAR, 'LEIA')
+        self.consome(tt.PVIRG, 'LEIA')
         
     #ATRIBUICAO → id atrib EXPR pvirg
     def ATRIBUICAO(self):
-        self.consome(tt.ID)
-        self.consome(tt.ATRIB)
+        self.consome(tt.ID, 'ATRIBUICAO')
+        self.consome(tt.ATRIB, 'ATRIBUICAO')
         self.EXPR()
-        self.consome(tt.PVIRG)
+        self.consome(tt.PVIRG, 'ATRIBUICAO')
        
     #ESCREVA → write abrepar LIST_W fechapar pvirg 
     def ESCREVA(self):
-        self.consome(tt.WRITE)
-        self.consome(tt.ABREPAR)
+        self.consome(tt.WRITE, 'ESCREVA')
+        self.consome(tt.ABREPAR, 'ESCREVA')
         self.LIST_W()
-        self.consome(tt.FECHAPAR)
-        self.consome(tt.PVIRG)
+        self.consome(tt.FECHAPAR, 'ESCREVA')
+        self.consome(tt.PVIRG, 'ESCREVA')
         
     #LIST_W → ELEM_W L
     def LIST_W(self):
@@ -252,7 +280,7 @@ class Sintatico:
     #L → virg LIST_W | λ
     def L(self):
         if self.tokenEsperadoEncontrado(tt.VIRG):
-            self.consome(tt.VIRG)
+            self.consome(tt.VIRG, 'L')
             self.LIST_W()
         else:
             pass
@@ -264,7 +292,7 @@ class Sintatico:
            self.tokenEsperadoEncontrado( tt.FALSE ) or self.tokenEsperadoEncontrado(tt.OPNEG):
             self.EXPR()
         else:
-            self.consome(tt.CADEIA)
+            self.consome(tt.CADEIA, 'ELEM_W')
         
             
     #EXPR → SIMPLES P
@@ -275,7 +303,7 @@ class Sintatico:
     #P → oprel SIMPLES | λ
     def P(self):
         if self.tokenEsperadoEncontrado(tt.OPREL):
-            self.consome(tt.OPREL)
+            self.consome(tt.OPREL, 'P')
             self.SIMPLES()
         else:
             pass
@@ -288,7 +316,7 @@ class Sintatico:
     #R → opad SIMPLES | λ
     def R(self):
         if self.tokenEsperadoEncontrado(tt.OPAD):
-            self.consome(tt.OPAD)
+            self.consome(tt.OPAD, 'R')
             self.SIMPLES()
         else:
             pass
@@ -301,7 +329,7 @@ class Sintatico:
     #S → opmul TERMO | λ
     def S(self):
         if self.tokenEsperadoEncontrado(tt.OPMUL):
-            self.consome(tt.OPMUL)
+            self.consome(tt.OPMUL, 'S')
             self.TERMO()
         else:
             pass
@@ -309,20 +337,20 @@ class Sintatico:
     #FAT → id | cte | abrepar EXPR fechapar | true | false | opneg FAT
     def FAT(self):
         if self.tokenEsperadoEncontrado( tt.OPNEG ):
-            self.consome(tt.OPNEG)
+            self.consome(tt.OPNEG, 'FAT')
             self.FAT()
         elif self.tokenEsperadoEncontrado( tt.CTE ):
-            self.consome(tt.CTE)
+            self.consome(tt.CTE, 'FAT')
         elif self.tokenEsperadoEncontrado( tt.ABREPAR ):
-            self.consome(tt.ABREPAR)
+            self.consome(tt.ABREPAR, 'FAT')
             self.EXPR()
-            self.consome(tt.FECHAPAR)
+            self.consome(tt.FECHAPAR, 'FAT')
         elif self.tokenEsperadoEncontrado( tt.TRUE ):
-            self.consome(tt.TRUE)
+            self.consome(tt.TRUE, 'FAT')
         elif self.tokenEsperadoEncontrado( tt.FALSE ):
-            self.consome(tt.FALSE)
+            self.consome(tt.FALSE, 'FAT')
         else:
-            self.consome(tt.ID)
+            self.consome(tt.ID, 'FAT')
         
 
 if __name__== "__main__":
